96411f17fecfecc2ae0d91cde7cd33019755be01
Pat <113007904+PattZac@users.noreply.github.com>
2022-09-28 09:27:07 +0200

    Add files via upload

---
+++ RadioButtonKontrola/RadioButtonKontrola/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadioButtonKontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Text = "Odabrali ste prijevoz " + Environment.NewLine;
+            if (radioButtonAvion.Checked ==true) { Text += "Avion"; }
+            if (radioButtonAutobus.Checked == true) { Text += "Autobus"; }
+            if (radioButtonVlastiti.Checked == true) { Text += "Vlastiti prijevoz"; }
+            textBox1.Text = Text;
+        }
+    }
+}
